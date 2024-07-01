0145557ba975951a74f6a07c3583bba8b4550a96
Israel <israelnavarreterodriguez@gmail.com>
2024-07-01 16:11:35 +0200

    Creación del proyecto

---
+++ src/main/kotlin/com/example/jsonplaceholderbcnc/JsonplaceholderbcncApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jsonplaceholderbcnc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsonplaceholderbcncApplication
+
+fun main(args: Array<String>) {
+	runApplication<JsonplaceholderbcncApplication>(*args)
+}
